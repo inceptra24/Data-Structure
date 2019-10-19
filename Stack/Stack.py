ee5d710e881403979a0cdabc11a1a44a3d7908e8
Ajumal <ajupazhamayil@users.noreply.github.com>
2019-10-19 11:24:00 +0530

    Stack in python

---
+++ Stack/Stack.py
@@ -0,0 +1,10 @@
+# Stack implementation using 
+# Python pop operation in list
+stack = ["Ajmal", "Jyothis", "Vaishnav"] 
+stack.append("Ram") 
+stack.append("Iqbal") 
+print(stack) 
+print(stack.pop()) 
+print(stack) 
+print(stack.pop()) 
+print(stack) 
